be6a4e08a063aaa23c2c1e974ec4aa55760b3336
Maxime <maximecattet@gmx.com>
2016-08-08 18:46:56 +0200

    add volumes and external links processing

--- index.js
+++ index.js
@@ -6,19 +6,83 @@ var chalk = require('chalk')
 
 var srcpath = './docker'
 
-//let's find all docker-compose.yml file
-var array = fs.readdirSync(srcpath).filter(function(file){
+if (process.argv[2] !== undefined) {
+    srcpath = process.argv[2]
+}
+
+console.log("docker folder: " + chalk.blue(srcpath));
+
+//let's find all folders inside srcpath
+var folders = fs.readdirSync(srcpath).filter(function(file){
     return fs.statSync(path.join(srcpath, file)).isDirectory()
 })
 
-array.forEach(function(folder) {
+console.log(chalk.blue(folders.length) + ' folders found');
+console.log('will try to read ' + chalk.gray("'docker-compose.yml'") + ' files');
+
+var fatCompose = {}
+
+folders.forEach(function(folder) {
     try {
-        var dockerCompose = yaml.safeLoad(fs.readFileSync(srcpath + '/' + folder + '/docker-compose.yml'))
-        var dockerName = Object.keys(dockerCompose)[0]
-        console.log(dockerCompose[dockerName].container_name);
+        var compose = yaml.safeLoad(fs.readFileSync(srcpath + '/' + folder + '/docker-compose.yml'))
+        var dockerName = Object.keys(compose)[0]
+
+        if (compose[dockerName].hasOwnProperty('volumes')) {
+            compose[dockerName].volumes = updateVolumes(compose[dockerName].volumes, folder)
+        }
+        updateExternalLinks(compose, dockerName)
+        fatCompose[dockerName] = compose[dockerName]
     } catch (e) {
         console.log(chalk.red(e));
     } finally {
-
+        console.log(dockerName + ' ' + chalk.green('done'));
     }
 })
+
+console.log(chalk.green("OK") + ': add all docker-compose to a big one');
+
+try {
+    console.log("will write the new " + chalk.gray("'docker-compose.yml'"));
+    var dump = yaml.safeDump(fatCompose)
+    fs.writeFileSync('docker-compose.yml', dump)
+} catch (e) {
+    console.log(chalk.red(e));
+} finally {
+    console.log(chalk.green('Successfully merged your docker-compose.yml!'));
+}
+
+//Add folder's name to the file or folder path when passing volume to the container
+// return the new volumes array
+function updateVolumes(volumes, folderName) {
+    var newVolumes = []
+    volumes.forEach(function(volume) {
+        if (volume.startsWith("./")) {
+            var newVolumePath = './' + folderName + '/' + volume.slice(2, volume.length - 1)
+            newVolumes.push(newVolumePath)
+        }
+
+        if (volume.startsWith('../')) {
+            newVolumes.push(volume.slice(1, volume.length - 1))
+        }
+    })
+
+    return newVolumes
+}
+
+// Void function that will transfert container links from 'external_links' to 'links'
+function updateExternalLinks(compose, dockerName) {
+    if (!compose[dockerName].hasOwnProperty("links")) {
+        compose[dockerName].links = []
+    }
+
+    if (compose[dockerName].hasOwnProperty("external_links")){
+        compose[dockerName].external_links.forEach(function(externalLink) {
+            compose[dockerName].links.push(externalLink)
+        })
+        delete compose[dockerName].external_links
+    }
+
+    if (compose[dockerName].links.length == 0) {
+        delete compose[dockerName].links
+    }
+}
